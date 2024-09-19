dd0760be8cc38213174c5ef9ff3f2d6254810b53
Nicholas Harrison <105819323+nicholasoharrison@users.noreply.github.com>
2024-09-19 18:09:34 -0400

    Began creating registration

--- MyPassApplication/urls.py
+++ MyPassApplication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('vault/', views.vault, name='vault'),
     path('create-password/', views.create_password, name='create_password'),
+    path('register/', views.register, name='register'),
 ]
--- MyPassApplication/views.py
+++ MyPassApplication/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
-
+@login_required
 def account(request):
     return render(request, 'account.html')
 
@@ -13,3 +15,13 @@ def vault(request):
 
 def create_password(request):
     return render(request, 'create_password.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
