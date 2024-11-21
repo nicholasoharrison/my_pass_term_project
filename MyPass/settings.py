45d361203d26431a51bbaa47129bbe4f8c255093
Yuliya Wickens <83892913+YulenkaW@users.noreply.github.com>
2024-11-21 22:10:04 +0000

    updated encription/decription, added new external library, fixed ui, still in the process decription for saved passwords only

--- MyPass/settings.py
+++ MyPass/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+from cryptography.fernet import Fernet
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zgvuic&hp41e4!@nrs)-hv_eu@mxo5o2nv7yfl!gxm7$q&xf=-'
+
+# Encryption Key for Passwords
+ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY', 'ztPF3wqDiNOxcmSQQa-C4-TGAXavFwjtPnUsTiLskZI=')
+cipher_suite = Fernet(ENCRYPTION_KEY)
+
+# Django's SECRET_KEY (this is required by Django for security)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your_secure_django_secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
