635c308d0ff894a5953f39dee2425edf86f2f643
msepse <114550628+msepse@users.noreply.github.com>
2024-11-26 02:38:07 -0500

    Added files generated from quick test run

--- MyPassApplication/apps.py
+++ MyPassApplication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from .mediators import UIMediator
-from .components import SavedPasswordsComponent
+from .components import SavedPasswords
 
 class MypassapplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -8,4 +8,4 @@ class MypassapplicationConfig(AppConfig):
 
 # Initialize Mediator and Components
 mediator = UIMediator()
-saved_passwords_component = SavedPasswordsComponent(mediator)
+saved_passwords_component = SavedPasswords(mediator)
